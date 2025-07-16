85b2c3a83ddfe4523511f69b78a937309064635e
Viešpirkiai <github@viespirkiai.top>
2025-07-16 14:49:39 +0300

    Papildyti spec asmens kodo atvejai

--- routes/asmuo.js
+++ routes/asmuo.js
@@ -19,18 +19,35 @@ asmuoRouter.get("/:id", async (req, res) => {
 
 	if (jarRezultatai.length === 0) {
 		const specAtvejai = {
+			801: {
+				pavadinimas: "CVP IS pilietis",
+				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas piliečiui."
+			},
+			802: {
+				pavadinimas: "CVP IS ūkininkas",
+				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas ūkininkui."
+			},
+			803: {
+				pavadinimas: "CVP IS užsienio įmonė",
+				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas užsienio įmonei."
+			},
+			804: {
+				pavadinimas: "CVP IS Lietuvos respublikos ambasada",
+				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas Lietuvos respublikos ambasadai."
+			},
 			807: {
 				pavadinimas: "CVP IS kitas asmuo",
 				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas kitam asmeniui."
+			},
+			808: {
+				pavadinimas: "CVP IS Europos komisijos atstovybė Lietuvoje",
+				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas Europos komisijos atstovybei Lietuvoje."
 			},
 			809: {
 				pavadinimas: "CVP IS fizinis asmuo",
 				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas fiziniam asmeniui."
 			},
-			803: {
-				pavadinimas: "CVP IS užsienio įmonė",
-				aprasymas: "Juridinis asmuo kurio ieškote neegzistuoja, kadangi tai yra tiesiog CVP IS sistemoje naudojamas kodas užsienio įmonei."
-			}
+			
 		};
 
 		if (specAtvejai[id]) {
