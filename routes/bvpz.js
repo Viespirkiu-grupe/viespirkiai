94affed91366e99cbe6a3d537cafc1e5e291322a
Viešpirkiai <github@viespirkiai.top>
2025-08-25 22:56:21 +0300

    Pridėti BVPŽ paieškos įrankį

---
+++ routes/bvpz.js
@@ -0,0 +1,23 @@
+import express from "express";
+import config from "../utils/config.js";
+import { serveOpenGraphImage } from "../utils/openGraphImage.js";
+
+const bvpzRouter = express.Router();
+
+bvpzRouter.get("/bvpz", async (req, res) => {
+    res.render("bvpz", {
+        customHead: config.customHead,
+    });
+});
+
+bvpzRouter.get("/bvpz.png", async (req, res) => {
+    return await serveOpenGraphImage(
+        res,
+        "Viešpirkiai pilietinė iniciatyva",
+        "BVPŽ kodų paieška",
+        "",
+        "viespirkiai.top/bvpz",
+    );
+});
+
+export default bvpzRouter;
