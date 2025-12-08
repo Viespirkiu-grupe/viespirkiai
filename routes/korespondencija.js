2f36462283abd274be84f3d43afec3849043f916
Viešpirkiai <github@viespirkiai.top>
2025-12-08 22:01:20 +0200

    Panaikinti redirectą į korespondenciją (puslapio nebėra)

--- routes/korespondencija.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const korespondencijaRouter = express.Router();
-
-korespondencijaRouter.get("/korespondencija", async (req, res) => {
-    res.redirect("https://github.com/Viespirkiu-grupe/korespondencija/issues");
-});
-
-export default korespondencijaRouter;
