0c2b4ea2e83c76fc0fc8f08ad3cadc83b0a5374c
Viešpirkiai <github@viespirkiai.top>
2026-01-09 08:36:57 +0200

    Paspartinti OCR rezervacijų panaikinimą

--- tasks/ocr/pravalytiRezervacijas.js
+++ tasks/ocr/pravalytiRezervacijas.js
@@ -2,16 +2,43 @@ import { postgres } from "../../postgres/postgres.js";
 import { log } from "../../utils/log.js";
 
 /**
- * Išvalo rezervuotas OCR užduotis, kurios buvo rezervuotos daugiau nei prieš 6 valandas.
+ * Išvalo rezervuotas OCR užduotis, kurios buvo rezervuotos daugiau nei prieš 3 valandas.
+ * Vykdo po 100 eilučių vienu metu.
  */
 export async function pravalytiOcrRezervacijas() {
-    await postgres.query(`UPDATE failai
-    SET
-        "ocrState" = 0,
-        "ocrLockTimestamp" = NULL,
-        "ocrNode" = NULL
-    WHERE "ocrState" = -3
-      AND "ocrLockTimestamp" <= (now() AT TIME ZONE 'Europe/Vilnius' - interval '6 hours');
-`);
-    log(`Išvalytos rezervuotos OCR užduotys.`);
+    const limit = 10;
+
+    const res = await postgres.query(
+        `
+        WITH to_update AS (
+            SELECT "id"
+            FROM failai
+            WHERE "ocrState" = -3
+              AND "ocrLockTimestamp" <= (now() AT TIME ZONE 'Europe/Vilnius' - interval '1 hours')
+            LIMIT $1
+        )
+        UPDATE failai f
+        SET
+            "ocrState" = 0,
+            "ocrLockTimestamp" = NULL,
+            "ocrNode" = NULL
+        FROM to_update t
+        WHERE f."id" = t."id"
+        RETURNING f."id";
+    `,
+        [limit],
+    );
+
+    return res.rowCount;
+}
+
+// jei vykdomas tiesiogiai
+if (import.meta.url === `file://${process.argv[1]}`) {
+    (async () => {
+        let updated;
+        do {
+            updated = await pravalytiOcrRezervacijas();
+            log(`Išvalytos ${updated} rezervuotos OCR užduotys.`);
+        } while (updated > 0);
+    })();
 }
